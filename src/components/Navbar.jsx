e869b81d3dea30143a82c8a85d60324b42da8b9b
httpcyberdata <shieldedsafety@gmail.com>
2023-04-14 11:11:15 -0400

    chore: created Navbar.jsx with list items and a h1 header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="text-white">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <ul>
+            <li>Home</li>
+            <li>Company</li>
+            <li>Resources</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
