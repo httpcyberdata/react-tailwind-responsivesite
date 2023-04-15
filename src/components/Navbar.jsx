005409575af2a4aa60b4a06f58e783578b898b29
httpcyberdata <shieldedsafety@gmail.com>
2023-04-14 20:45:03 -0400

    chore: added react-icons, hid ul to show AiOutlineMenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div className="text-white">
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
-        <ul>
-            <li>Home</li>
-            <li>Company</li>
-            <li>Resources</li>
-            <li>About</li>
-            <li>Contact</li>
+        <ul className="flex hidden">
+            <li className="p-4">Home</li>
+            <li className="p-4">Company</li>
+            <li className="p-4">Resources</li>
+            <li className="p-4">About</li>
+            <li className="p-4">Contact</li>
         </ul>
+        <div>
+            <AiOutlineMenu />
+        </div>
     </div>
   )
 }
