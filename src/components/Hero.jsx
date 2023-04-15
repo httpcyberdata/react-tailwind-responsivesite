d39fb96da412485ead531b31d70a22c7ca48c23f
httpcyberdata <shieldedsafety@gmail.com>
2023-04-15 13:32:31 -0400

    chore: created Hero.jsx with h1 and p tag

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+        <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            <p className="text-[#00df9a] font-bold p-2">GAMING WITH DATA ANALYTICS</p>
+            <h1 className="md:text-7xl sm:text-6xl text-4xl">Grow with data.</h1>
+            <div>
+                <p>Fast, flexible financing</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
