5985ba7bb8e30972743b764459c35b393ab20477
httpcyberdata <shieldedsafety@gmail.com>
2023-04-18 10:12:01 -0400

    chore: make double tier stand out, double tier button is inverted

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,16 +14,59 @@ const Cards = () => {
                         <h2 className="text-2xl font-bold text-center py-8">
                             Sinlge User
                         </h2>
-                        <p>
-                            $145
+                        <p className="text-center text-4xl font-bold">
+                            $145 
                         </p>
-                        <div>
-                            <p>500 GB Storage</p>
-                            <p>1 Granted User</p>
-                            <p>Send up to 2 GB</p>
+                        <div className="text-center font-medium">
+                            <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
+                            <p className="py-2 border-b mx-8">1 Granted User</p>
+                            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
                         </div>
-                        <button>Start Trial</button>
-                    </div>                    
+                        <button
+                            className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3"
+                        >
+                            Start Trial
+                        </button>
+                    </div> 
+                    <div className="w-full shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale(105) duration-300">
+                        <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Double} alt=""/>
+                        <h2 className="text-2xl font-bold text-center py-8">
+                            Double User
+                        </h2>
+                        <p className="text-center text-4xl font-bold">
+                            $145 
+                        </p>
+                        <div className="text-center font-medium">
+                            <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
+                            <p className="py-2 border-b mx-8">1 Granted User</p>
+                            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
+                        </div>
+                        <button
+                            className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3"
+                        >
+                            Start Trial
+                        </button>
+                    </div> 
+                    <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale(105) duration-300">
+                        <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Triple} alt="" />
+                        <h2 className="text-2xl font-bold text-center py-8">
+                            Triple User
+                        </h2>
+                        <p className="text-center text-4xl font-bold">
+                            $145 
+                        </p>
+                        <div className="text-center font-medium">
+                            <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
+                            <p className="py-2 border-b mx-8">1 Granted User</p>
+                            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
+                        </div>
+                        <button
+                            className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3"
+                        >
+                            Start Trial
+                        </button>
+                    </div> 
+                                       
             </div>
         </div>
     )
