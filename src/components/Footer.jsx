2c75f4f26d12908cd95a9e62c6b4ee0ef0044a08
httpcyberdata <shieldedsafety@gmail.com>
2023-04-19 20:43:15 -0400

    chore: added social media icons to footer and services to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import {
     FaDribbbleSquare,
-    FaFacebookSqaure,
-    FaGitHubSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
     FaInstagram,
     FaTwitterSquare
 } from 'react-icons/fa';
@@ -14,7 +14,25 @@ const Footer = () => {
             <h1 className="w-full text-3xl font-bold text-[#00df9a]">
                  REACT    
             </h1>
-            <p>Lo rem ipsum dolor sit amet consectetur adipisicing elit. Ipsum perspiciatis ipsa dolorum laudantium doloremque. Sed pariatur quia blanditiis exercitationem nesciunt!</p>
+            <p className="py-4">Lo rem ipsum dolor sit amet consectetur adipisicing elit. Ipsum perspiciatis ipsa dolorum laudantium doloremque. Sed pariatur quia blanditiis exercitationem nesciunt!</p>
+            <div className="flex justify-between md:w-[75%] my-6">
+                <FaFacebookSquare size={30} />
+                <FaInstagram size={30} />
+                <FaTwitterSquare size={30} />
+                <FaGithubSquare size={30} />
+                <FaDribbbleSquare size={30} />
+            </div>
+        </div>
+        <div className="lg:col-span-3 flex justify-between">
+            <div>
+                <h2 className="font-medium text-gray-400">Solutions</h2>
+                <ul>
+                    <li className="py-2 text-sm">Analytics</li>
+                    <li className="py-2 text-sm">Marketing</li>
+                    <li className="py-2 text-sm">Commerce</li>
+                    <li className="py-2 text-sm">Insights</li>
+                </ul>
+            </div>
         </div>
     </div>
   );
